133b3416e5565ddb4989948261b6b778afacd1f6
Njokatem Comfort Mbeng <49481570+Mbengparis@users.noreply.github.com>
2020-02-07 22:48:48 +0100

    Add files via upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css'
 
 
@@ -39,8 +40,10 @@ onSearchChange=(event) =>{
             <div className='tc'>
             <h1 className='f1'>Robofriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
-            <Scroll>  
-            <CardList robots = {filteredRobots}/>
+            <Scroll> 
+                <ErrorBoundary>
+                 <CardList robots = {filteredRobots}/>
+                </ErrorBoundary> 
             </Scroll>  
             </div>
         )
